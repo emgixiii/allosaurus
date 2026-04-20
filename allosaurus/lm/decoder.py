f02c3f9a81e4dfd810a7b23726aeffaae278480d
emgixiii <emgi@live.in>
2026-04-20 23:17:28 +0530

    feat: output blanks as .

--- allosaurus/lm/decoder.py
+++ allosaurus/lm/decoder.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from itertools import groupby
 import numpy as np
 
-class PhoneDecoder:
 
+class PhoneDecoder:
     def __init__(self, model_path, inference_config):
         """
         This class is an util for decode both phones and words
@@ -37,26 +37,8 @@ class PhoneDecoder:
 
         logits = mask.mask_logits(logits)
 
-        emit_frame_idx = []
-
-        cur_max_arg = -1
-
-        # find all emitting frames
-        for i in range(len(logits)):
-
-            logit = logits[i]
-            logit[0] /= emit
-
-            arg_max = np.argmax(logit)
-
-            # this is an emitting frame
-            if arg_max != cur_max_arg and arg_max != 0:
-                emit_frame_idx.append(i)
-                cur_max_arg = arg_max
-
-        # decode all emitting frames
         decoded_seq = []
-        for idx in emit_frame_idx:
+        for idx in range(len(logits)):
             logit = logits[idx]
             exp_prob = np.exp(logit - np.max(logit))
             probs = exp_prob / exp_prob.sum()
@@ -64,18 +46,25 @@ class PhoneDecoder:
             top_phones = logit.argsort()[-topk:][::-1]
             top_probs = sorted(probs)[-topk:][::-1]
 
-            stamp = f"{self.config.window_shift*idx:.3f} {self.config.window_size:.3f} "
+            stamp = (
+                f"{self.config.window_shift * idx:.3f} {self.config.window_size:.3f} "
+            )
 
             if topk == 1:
-
-                phones_str = ' '.join(mask.get_units(top_phones))
+                phones_str = "".join(
+                    m if idx != 0 else "."
+                    for idx, m in zip(top_phones, mask.get_units(top_phones))
+                )
                 if timestamp:
                     phones_str = stamp + phones_str
 
                 decoded_seq.append(phones_str)
             else:
-                phone_prob_lst = [f"{phone} ({prob:.3f})" for phone, prob in zip(mask.get_units(top_phones), top_probs)]
-                phones_str = ' '.join(phone_prob_lst)
+                phone_prob_lst = [
+                    f"{phone} ({prob:.3f})"
+                    for phone, prob in zip(mask.get_units(top_phones), top_probs)
+                ]
+                phones_str = " ".join(phone_prob_lst)
 
                 if timestamp:
                     phones_str = stamp + phones_str
@@ -83,10 +72,11 @@ class PhoneDecoder:
                 decoded_seq.append(phones_str)
 
         if timestamp:
-            phones = '\n'.join(decoded_seq)
+            phones = "\n".join(decoded_seq)
         elif topk == 1:
-            phones = ' '.join(decoded_seq)
+            phones = "".join(decoded_seq)
         else:
-            phones = ' | '.join(decoded_seq)
+            phones = " | ".join(decoded_seq)
 
         return phones
+
